2757a41d902f1e898539021622b27c717b860025
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2017-06-11 21:48:03 +0300

    first commit

---
+++ TotalCommander/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.driverList = new System.Windows.Forms.ListView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.driverList2 = new System.Windows.Forms.ListView();
+            this.listView3 = new System.Windows.Forms.ListView();
+            this.path1 = new System.Windows.Forms.TextBox();
+            this.path2 = new System.Windows.Forms.TextBox();
+            this.backButton = new System.Windows.Forms.Button();
+            this.backButton2 = new System.Windows.Forms.Button();
+            this.create1 = new System.Windows.Forms.Button();
+            this.create2 = new System.Windows.Forms.Button();
+            this.file1 = new System.Windows.Forms.Button();
+            this.file2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // driverList
+            // 
+            this.driverList.Location = new System.Drawing.Point(12, 32);
+            this.driverList.Name = "driverList";
+            this.driverList.Size = new System.Drawing.Size(168, 135);
+            this.driverList.TabIndex = 0;
+            this.driverList.UseCompatibleStateImageBehavior = false;
+            this.driverList.View = System.Windows.Forms.View.List;
+            this.driverList.SelectedIndexChanged += new System.EventHandler(this.DriverList_SelectedIndexChanged);
+            this.driverList.MouseClick += new System.Windows.Forms.MouseEventHandler(this.DriveOnClick);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(186, 32);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(286, 272);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // driverList2
+            // 
+            this.driverList2.Location = new System.Drawing.Point(524, 32);
+            this.driverList2.Name = "driverList2";
+            this.driverList2.Size = new System.Drawing.Size(178, 135);
+            this.driverList2.TabIndex = 2;
+            this.driverList2.UseCompatibleStateImageBehavior = false;
+            this.driverList2.View = System.Windows.Forms.View.List;
+            this.driverList2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.DriveOnClick2);
+            // 
+            // listView3
+            // 
+            this.listView3.Location = new System.Drawing.Point(708, 32);
+            this.listView3.Name = "listView3";
+            this.listView3.Size = new System.Drawing.Size(296, 272);
+            this.listView3.TabIndex = 3;
+            this.listView3.UseCompatibleStateImageBehavior = false;
+            this.listView3.View = System.Windows.Forms.View.List;
+            this.listView3.SelectedIndexChanged += new System.EventHandler(this.listView3_SelectedIndexChanged);
+            // 
+            // path1
+            // 
+            this.path1.Location = new System.Drawing.Point(13, 360);
+            this.path1.Name = "path1";
+            this.path1.Size = new System.Drawing.Size(459, 22);
+            this.path1.TabIndex = 4;
+            // 
+            // path2
+            // 
+            this.path2.Location = new System.Drawing.Point(524, 360);
+            this.path2.Name = "path2";
+            this.path2.Size = new System.Drawing.Size(480, 22);
+            this.path2.TabIndex = 5;
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(12, 410);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(75, 23);
+            this.backButton.TabIndex = 6;
+            this.backButton.Text = "Back";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // backButton2
+            // 
+            this.backButton2.Location = new System.Drawing.Point(524, 410);
+            this.backButton2.Name = "backButton2";
+            this.backButton2.Size = new System.Drawing.Size(75, 23);
+            this.backButton2.TabIndex = 7;
+            this.backButton2.Text = "Back";
+            this.backButton2.UseVisualStyleBackColor = true;
+            this.backButton2.Click += new System.EventHandler(this.backButton2_Click);
+            // 
+            // create1
+            // 
+            this.create1.Location = new System.Drawing.Point(13, 191);
+            this.create1.Name = "create1";
+            this.create1.Size = new System.Drawing.Size(167, 23);
+            this.create1.TabIndex = 8;
+            this.create1.Text = "Create Folder";
+            this.create1.UseVisualStyleBackColor = true;
+            this.create1.Click += new System.EventHandler(this.create1_Click);
+            // 
+            // create2
+            // 
+            this.create2.Location = new System.Drawing.Point(524, 190);
+            this.create2.Name = "create2";
+            this.create2.Size = new System.Drawing.Size(178, 23);
+            this.create2.TabIndex = 10;
+            this.create2.Text = "Create Folder";
+            this.create2.UseVisualStyleBackColor = true;
+            this.create2.Click += new System.EventHandler(this.create2_Click);
+            // 
+            // file1
+            // 
+            this.file1.Location = new System.Drawing.Point(12, 221);
+            this.file1.Name = "file1";
+            this.file1.Size = new System.Drawing.Size(168, 23);
+            this.file1.TabIndex = 11;
+            this.file1.Text = "Create File";
+            this.file1.UseVisualStyleBackColor = true;
+            this.file1.Click += new System.EventHandler(this.file1_Click);
+            // 
+            // file2
+            // 
+            this.file2.Location = new System.Drawing.Point(524, 220);
+            this.file2.Name = "file2";
+            this.file2.Size = new System.Drawing.Size(178, 23);
+            this.file2.TabIndex = 12;
+            this.file2.Text = "Create File ";
+            this.file2.UseVisualStyleBackColor = true;
+            this.file2.Click += new System.EventHandler(this.file2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1111, 616);
+            this.Controls.Add(this.file2);
+            this.Controls.Add(this.file1);
+            this.Controls.Add(this.create2);
+            this.Controls.Add(this.create1);
+            this.Controls.Add(this.backButton2);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.path2);
+            this.Controls.Add(this.path1);
+            this.Controls.Add(this.listView3);
+            this.Controls.Add(this.driverList2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.driverList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView driverList;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView driverList2;
+        private System.Windows.Forms.ListView listView3;
+        private System.Windows.Forms.TextBox path1;
+        private System.Windows.Forms.TextBox path2;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button backButton2;
+        private System.Windows.Forms.Button create1;
+        private System.Windows.Forms.Button create2;
+        private System.Windows.Forms.Button file1;
+        private System.Windows.Forms.Button file2;
+    }
+}
+
---
+++ TotalCommander/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //driver instatiation
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (DriveInfo di in DriveInfo.GetDrives())
+            {
+                driverList.Items.Add(di.Name);
+                driverList2.Items.Add(di.Name);
+            }
+        }
+
+        private void DriverList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //First drive on Click
+        private void DriveOnClick(object sender, MouseEventArgs e)
+        {
+            listView1.Clear();
+            string help = driverList.SelectedItems[0].Text;
+
+            foreach (String director in Directory.GetDirectories(help))
+            {
+                listView1.Items.Add(new FileInfo(director).Name);
+            }
+
+            path1.Text = help;
+        }
+
+        //Second drive on click
+        private void DriveOnClick2(object sender, MouseEventArgs e)
+        {
+            listView3.Clear();
+            string help = driverList2.SelectedItems[0].Text;
+
+            foreach (String director in Directory.GetDirectories(help))
+            {
+                listView3.Items.Add(new FileInfo(director).Name);
+            }
+            path2.Text = help;
+        }
+
+        //a helpful method
+
+        private string spliting(string split)
+        {
+            int p = 0;
+            for (int i = split.Length - 2; i >= 0; i--)
+            {
+                if (split[i] == '\\')
+                {
+                    p = i + 1;
+                    break;
+                }
+                if (split[i] == ':')
+                    return split;
+            }
+            split = split.Remove(p);
+            return split;
+        }
+
+        //backButton 2
+
+        private void backButton2_Click(object sender, EventArgs e)
+        {
+            path2.Text = spliting(path2.Text);
+            listView3.Clear();
+            foreach (String director in Directory.GetDirectories(path2.Text))
+            {
+                listView3.Items.Add(new FileInfo(director).Name);
+            }
+            foreach (String director in Directory.GetFiles(path2.Text, "*.txt"))
+            {
+                listView3.Items.Add(new FileInfo(director).Name);
+            }
+
+        }
+
+        //backButton 1
+        private void backButton_Click(object sender, EventArgs e)
+        {
+
+            path1.Text = spliting(path1.Text);
+            listView1.Clear();
+            foreach (String director in Directory.GetDirectories(path1.Text))
+            {
+                listView1.Items.Add(new FileInfo(director).Name);
+            }
+            foreach (String director in Directory.GetFiles(path1.Text, "*.txt"))
+            {
+                listView1.Items.Add(new FileInfo(director).Name);
+            }
+
+        }
+
+        //ListView1
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            string help = listView1.SelectedItems[0].Text;
+            if (help.Contains(".txt"))
+            {
+                Process.Start(path1.Text + help);
+            }
+            else
+            {
+                listView1.Clear();
+                foreach (String director in Directory.GetDirectories(path1.Text + help))
+                {
+                    listView1.Items.Add(new FileInfo(director).Name);
+                }
+                foreach (String director in Directory.GetFiles(path1.Text + help, "*.txt"))
+                {
+                    listView1.Items.Add(new FileInfo(director).Name);
+                }
+                path1.Text = path1.Text + help + "\\";
+            }
+        }
+
+        //listview3
+
+        private void listView3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string help = listView3.SelectedItems[0].Text;
+            if (help.Contains(".txt"))
+            {
+                Process.Start(path2.Text + help);
+            }
+            else
+            {
+                listView3.Clear();
+                foreach (String director in Directory.GetDirectories(path2.Text + help))
+                {
+                    listView3.Items.Add(new FileInfo(director).Name);
+                }
+                foreach (String director in Directory.GetFiles(path2.Text + help, "*.txt"))
+                {
+                    listView3.Items.Add(new FileInfo(director).Name);
+                }
+                path2.Text = path2.Text + help + "\\";
+            }
+
+        }
+
+        //create Folder 1
+
+        private void create1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!Directory.Exists(path1.Text + "New Folder"))
+                {
+                    Directory.CreateDirectory(path1.Text + "New Folder");
+                    listView1.Items.Add("New Folder");
+                    if (path2.Text.CompareTo(path1.Text) == 0)
+                        listView3.Items.Add("New Folder");
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.GetBaseException();
+            }
+        }
+
+        //create Folder 2
+
+        private void create2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!Directory.Exists(path2.Text + "New Folder"))
+                {
+                    Directory.CreateDirectory(path2.Text + "New Folder");
+                    listView3.Items.Add("New Folder");
+                    if (path2.Text.CompareTo(path1.Text) == 0)
+                        listView1.Items.Add("New Folder");
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.GetBaseException();
+            }
+        }
+
+        //file create 1
+
+        private void file1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!File.Exists(path1.Text + "New Text File"))
+                {
+                    File.CreateText(path1.Text + "New Text File");
+                    listView1.Items.Add("New Text File.txt");
+                    if (path2.Text.CompareTo(path1.Text) == 0)
+                        listView3.Items.Add("New Text File.txt");
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.GetBaseException();
+            }
+        }
+
+       //file create 2
+
+        private void file2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!File.Exists(path1.Text + "New Text File"))
+                {
+                    File.CreateText(path2.Text + "New Text File");
+                    listView3.Items.Add("New Text File.txt");
+                    if (path2.Text.CompareTo(path1.Text) == 0)
+                        listView1.Items.Add("New Text File.txt");
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.GetBaseException();
+            }
+        }
+    }
+}
